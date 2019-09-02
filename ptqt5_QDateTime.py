ad86fc43bb796ebb47181befc13730eac939ba91
yeongjin <yeongjin13@gmail.com>
2019-09-02 15:10:32 +0900

    Create ptqt5_QDateTime.py
    pyqt5 날짜와 시간 표시하기(QDateTime)

---
+++ ptqt5_QDateTime.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QDate, Qt, QTime
+class MyApp(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.date = QDate.currentDate()
+		self.time = QTime.currentTime()
+		self.initUI()
+	def initUI(self):
+		self.statusBar().showMessage(self.date.toString(Qt.ISODate)+ " " + self.time.toString())
+
+		print(self.time.toString())
+
+		self.setWindowTitle('Date')
+		self.setGeometry(300, 300, 400, 200)
+		self.show()
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = MyApp()
+	sys.exit(app.exec_())
